36d48734cef1d5abf6a436e8d218e6519e6fb93e
Branda Sisoutham <brandas@unc.edu>
2023-09-26 21:46:39 -0400

    oh my jesis

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -3,18 +3,35 @@ __author__="730577554"
 
 secret_word = "python"
 i: int = len(secret_word)
+index: int = 0
+result: str = ""
 guess: str = input(f"what is your {i}-letter guess? ")
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
 
-while i != len(guess):
-    guess = input(f"That was not {i} letters! Try again: ")
-    if (guess) == (secret_word):
-         print(f"Woo! You got it!") 
-    if (guess) != (secret_word) and i == len(guess):
-        print(f"Not quite. Play again soon!")
-    
-    
+while len(guess) != len(secret_word):
+       guess = input(f"That was not {i} letters! Try again: ") 
 
+while index < len(secret_word):
+    if guess[index] == secret_word[index]:
+        result += GREEN_BOX
+    else:
+        char_exist = False
+        alt_indices = 0
+        while not char_exist and alt_indices < len(secret_word):
+            if secret_word[alt_indices] == guess[index]:
+                char_exist = True
+            else:
+                alt_indices += 1
+        if char_exist:
+            result += YELLOW_BOX
+        else:
+            result += WHITE_BOX
+    index += 1
+print(result)
 
-    
-    
-
+if guess == secret_word:
+    print(f"Woo! You got it!")
+else:
+    print(f"Not quite. Play again soon!")
